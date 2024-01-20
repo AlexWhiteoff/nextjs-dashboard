455164fd2b03a16dcdfb7515ec9be31e003e29ad
Білий Олександр <71928700+AlexWhiteoff@users.noreply.github.com>
2024-01-20 22:08:01 +0200

    added customers + metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import CustomersTable from '@/app/ui/customers/table';
+import { lusitana } from '@/app/ui/fonts';
+import Search from '@/app/ui/search';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
+import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+  return (
+    <div className="w-full">
+      <Suspense key={query} fallback={<TableRowSkeleton />}>
+        <CustomersTable query={query} />
+      </Suspense>
+    </div>
+  );
 }
